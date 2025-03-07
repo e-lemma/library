58256594b45b5464c45077c99c106999b48bbc3f
eyuale <157725293+e-lemma@users.noreply.github.com>
2025-03-07 20:28:02 +0000

    Rename properties, add sample books

--- main.js
+++ main.js
@@ -1,16 +1,25 @@
-const VALID_READ_STATUSES = ["Not read", "Currently Reading", "Read"];
+const VALID_STATUSES = ["Not read", "Currently Reading", "Read"];
 
-const myLibrary = [];
+const myLibrary = [
+  new Book("The Hobbit", "J.R.R. Tolkien", 310, "Not read"),
+  new Book("1984", "George Orwell", 328, "read"),
+  new Book("To Kill a Mockingbird", "Harper Lee", 281, "Currently Reading"),
+];
+
+function generateID() {
+  return crypto.randomUUID();
+}
 
 function Book(title, author, pages, readStatus) {
+  this.id = generateID();
   this.title = title;
   this.author = author;
   this.pages = pages;
-  this.readStatus = readStatus;
+  this.status = readStatus;
 }
 
-function addBookToLibrary(title, author, pages, readStatus, library) {
-  const newBook = new Book(title, author, pages, readStatus);
+function addBookToLibrary(title, author, pages, status, library) {
+  const newBook = new Book(title, author, pages, status);
   library.push(newBook);
 }
 
